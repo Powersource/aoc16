de95959c8c0b0fb8590e59a20eaf736a688b7801
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-12-13 17:08:20 +0100

    Try to get reading and hashmapping to work

--- six/src/main.rs
+++ six/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Answer: {}", solve("large.in", 8).unwrap());
+}
+
+fn solve(file_name: &str, line_len: i32) -> Result<&str, io::Error> {
+    let file = File::open(file_name)?;
+    let input = BufReader::new(file);
+    let freq_lists = Vec::new();
+    for _ in 0..line_len {
+        freq_lists.push(HashMap::new());
+    }
+    println!("{:?}", freq_lists.len());
+    for line in input.lines() {
+        println!("{:?}", line.unwrap());
+    }
+    Ok("ok")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn small() {
+        assert_eq!(solve("small.in", 6).unwrap(), "easter");
+    }
 }
